53d1d520db4d23dd4b97271754721790a3afa8f4
Maldon <maldonmeehan@gmail.com>
2016-06-17 14:32:49 -0700

    takeout address component and add css and images

--- js/scripts.js
+++ js/scripts.js
@@ -7,29 +7,13 @@
    this.extra = extra;
    this.price = "";
  }
-// contact constructor
- function Contact(first, last, street, city, state, zip) {
-   this.firstName = first;
-   this.firstName = first;
-   this.lastName = last;
-   this.street = street;
-   this.city = city;
-   this.state = state;
-   this.zip = zip;
- }
+
 // pizza prototype formula
  Pizza.prototype.pizzaPrice = function(){
    return this.size + this.meat + this.veg + this.extra;
  }
- // contact prototype formula
- Contact.prototype.fullName = function() {
-   return this.firstName + " " + this.lastName;
- }
- Contact.prototype.fullAddress = function() {
-   return this.street + "," + this.city + "," + this.state + "," + this.zip;
- }
-//user interface logic
 
+//user interface logic
 $(document).ready(function() {
   $("form#order-piza").submit(function(event){
     event.preventDefault();
